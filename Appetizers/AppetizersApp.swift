dfb377fbe768ccfdcc4fa2634fd631b2b38cd536
John Patrick Echavez <john.echavez@rococoglobaltech.com>
2024-06-14 13:49:20 +0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by John Patrick Echavez on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
